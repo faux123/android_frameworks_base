4fc0d134b65d386ba9882b66546b400e6324c83b
Flavio Costa <flavio.cdc@gmail.com>
2011-07-07 01:12:11 -0300

    motus: handle envelope and explorer keys
    Change-Id: I1371c16c31b12379e6312401df8cd44002f4d9cb

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1381,6 +1381,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return handleQuickKeys(win, keyCode, down, keyguardOn);
         }
 
+        if ("motus".equals(Build.DEVICE)
+                && (keyCode == KeyEvent.KEYCODE_ENVELOPE || keyCode == KeyEvent.KEYCODE_EXPLORER)) {
+            return handleQuickKeys(win, keyCode, down, keyguardOn);
+        }
+
         // Shortcuts are invoked through Search+key, so intercept those here
         if (mSearchKeyPressed) {
             if (down && repeatCount == 0 && !keyguardOn) {
@@ -1403,7 +1408,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     /**
-     * Quick Keys for Vision (HTC - G2)
+     * Quick Keys
      */
     private boolean handleQuickKeys(WindowState win, int code, boolean down, boolean keyguardOn) {
 
@@ -1427,6 +1432,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             break;
                         case KeyEvent.KEYCODE_USER3:
                             property = Settings.System.USER_DEFINED_KEY3_APP;
+                            break;
+                        case KeyEvent.KEYCODE_ENVELOPE:
+                            property = Settings.System.USER_DEFINED_KEY_ENVELOPE;
+                            break;
+                        case KeyEvent.KEYCODE_EXPLORER:
+                            property = Settings.System.USER_DEFINED_KEY_EXPLORER;
                             break;
                         default:
                             return false;
